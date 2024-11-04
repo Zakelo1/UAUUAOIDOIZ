f6101c22b0f9624b1dc0fafdd252ce44007c3bc6
Zakarya El Ouahabi <zakaryaelouahabi@ZE.local>
2024-11-04 22:24:58 +0100

    Mise à jour des fichiers App.js et Multiplication.js

--- src/Multiplication.js
+++ src/Multiplication.js
@@ -5,7 +5,23 @@ const Multiplication = () => {
     const [deuxiemeNombre, setDeuxiemeNombre] = useState("");
     const [produit, setProduit] = useState(null);
     const [historique, setHistorique] = useState([]);
+    const [select, setselect] = useState([]);
+    const selectionnermulti = () => {
+        const resulat = select.reduce((acc, index) => {
+            const partie = historique[index].split('=')[0].split('*');
+        return acc * (parseFloat(partie[0]) * parseFloat(partie[1]));
+        }, 1);
+    setProduit(resulat)
+    }
+    const selection = (index) => {
+        if (select.includes(index)){
+            setselect(select.filter(i => i !== index));
+        } 
+        else {
+            setselect([...select, index]);
+        }
 
+    } 
     const multiplier = () => {
         const resultat = premierNombre * deuxiemeNombre;
         setProduit(resultat);
@@ -40,10 +56,12 @@ const Multiplication = () => {
                 onChange={handleDeuxiemeNombreChange}
             />
             <button onClick={multiplier}>Multiplier</button>
-            {produit !== null && <p>Le produit est : {produit}</p>}
+            <button onClick={selectionnermulti}>Mutiplierlessss</button>
+            {produit !== null && <p>Le produit total est : {produit}</p>}
             <ul>
                 {historique.map((item, index) => (
-                    <li key={index}>
+                    <li key={index}> 
+                    <input type="checkbox" checked={select.includes(index)} onChange={()=> selection(index)} />
                         {item}
                         <button onClick={() => supprimer(index)}>Supprimer</button>
                     </li>
